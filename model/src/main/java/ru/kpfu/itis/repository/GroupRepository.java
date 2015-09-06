413c3287f5e900da6a1bb717b92989ee31d45a81
sanekesv <sanekesv@gmail.com>
2015-09-06 11:41:11 +0300

    clear code

--- model/src/main/java/ru/kpfu/itis/repository/GroupRepository.java
+++ model/src/main/java/ru/kpfu/itis/repository/GroupRepository.java
@@ -1,11 +1,8 @@
 package ru.kpfu.itis.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.kpfu.itis.model.AcademicGroup;
-
-import java.util.List;
 
 /**
  * Created by sanekesv on 01.08.15.
@@ -15,7 +12,4 @@ public interface GroupRepository extends CrudRepository<AcademicGroup, Long> {
     AcademicGroup findOneById(Long id);
 
     AcademicGroup findOneByTitle(String title);
-
-//    @Query("SELECT * FROM AcademicGroup AS g WHERE id>0")
-//    List<AcademicGroup> getAllGroupsNameObj();
 }
